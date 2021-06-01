76d6986aac6915675eaad5928c02fc7f048aea9a
Jason McElvain <jasonom2@yahoo.com>
2021-06-01 09:24:47 -0500

    deploying to henroku

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const socket = require('socket.io');
-let port = 3000;
+let port = process.env.PORT || 3000;
 app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
